1040e826bffb1731e02cd24ab62b7b1bf23ad581
rui <ruixue.fi@gmail.com>
2022-01-01 23:16:39 +0200

    #10 war generated eventual with version number

--- java/maven_archetype/ee7/jax-rs-2.0-with-ejb3.2/src/main/resources/archetype-resources/src/main/java/sample/SampleRestResource.java
+++ java/maven_archetype/ee7/jax-rs-2.0-with-ejb3.2/src/main/resources/archetype-resources/src/main/java/sample/SampleRestResource.java
@@ -3,11 +3,10 @@ package ${package}.sample;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/rest")
+@Path("/sample")
 @RequestScoped
 public class SampleRestResource {
   
